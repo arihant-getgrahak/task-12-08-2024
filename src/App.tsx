c2e3079998ff91f75d8f91e762f5e394722f12a9
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-08-12 05:01:04 +0000

    integrate api make single sigle page website make button to genrate random quote

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+
+type Quote = {
+  _id: string;
+  content: string;
+  author: string;
+  authorSlug: string;
+  length: number;
+  tags: string[];
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [randomQuote, setRandomQuote] = useState<Quote | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+  const api: string = "https://api.quotable.io";
+
+  async function fetchApi() {
+    setLoading(true);
+    try {
+      const res = await fetch(`${api}/random`);
+      if (!res.ok) {
+        throw new Error('Failed to fetch the quote');
+      }
+      const data: Quote = await res.json();
+      setRandomQuote(data);
+      setError(null);
+    } catch (err: any) {
+      setError(err.message);
+      setLoading(false)
+    } finally {
+      setLoading(false);
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <GetRandomQuote loading={loading} error={error} randomQuote={randomQuote} func={fetchApi} />
+  );
+}
+
+function GetRandomQuote(props: {
+  loading: boolean, error: string | null, randomQuote: Quote | null, func: () => void
+}) {
+  const { loading, error, randomQuote, func } = props
+  return (
+    <div className="App flex justify-center items-center flex-col h-screen gap-3">
+      {loading && <p>Loading...</p>}
+      {error && <p>Error: {error}</p>}
+      {randomQuote && (
+        <div className="quote w-3/6">
+          <p>{randomQuote.content}</p>
+          <p className='text-center mt-2'>- {randomQuote.author}</p>
+        </div>
+      )}
+      <button className="border-2 p-2 border-red-500" onClick={func}>Get Random Quote</button>
+    </div>
   )
 }
 
-export default App
+export default App;
